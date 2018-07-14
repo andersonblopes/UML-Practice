d9baaefcdd1f4ad029d294b460890684a0104c6f
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-14 14:36:15 -0300

    tests the first domain class - Catgories.

--- Conceptual Modeling with UML Class Diagrams/Project/lopes-modelagem-conceitual/src/main/java/com/lopes/cursomodelagem/resources/categoriaResource.java
+++ Conceptual Modeling with UML Class Diagrams/Project/lopes-modelagem-conceitual/src/main/java/com/lopes/cursomodelagem/resources/categoriaResource.java
@@ -1,16 +1,28 @@
 package com.lopes.cursomodelagem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lopes.cursomodelagem.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class categoriaResource {
 
 	@GetMapping
-	public String iniciar() {
-		return "REST OK!";
+	public List<Categoria> lista() {
+		Categoria c1 = new Categoria(1L, "Computador");
+		Categoria c2 = new Categoria(2L, "Celular");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+
+		return lista;
 	}
 
 }
