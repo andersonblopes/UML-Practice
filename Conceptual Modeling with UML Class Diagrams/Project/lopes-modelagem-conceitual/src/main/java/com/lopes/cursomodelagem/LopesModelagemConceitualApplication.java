31fce323d9ff3c2845eed1a76517045bbda21396
Anderson Lopes <anderson.Lopes@adtalem.com>
2018-07-14 14:06:49 -0300

    Create project.

---
+++ Conceptual Modeling with UML Class Diagrams/Project/lopes-modelagem-conceitual/src/main/java/com/lopes/cursomodelagem/LopesModelagemConceitualApplication.java
@@ -0,0 +1,12 @@
+package com.lopes.cursomodelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LopesModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LopesModelagemConceitualApplication.class, args);
+	}
+}
